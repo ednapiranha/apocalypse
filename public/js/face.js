1e24345aa4dc99f1e84b013c23996c45dffced5a
Jen <jfong@mozilla.com>
2016-04-10 20:59:33 -0700

    some adjustments to the planet styles

--- public/js/face.js
+++ public/js/face.js
@@ -6,8 +6,11 @@ const OrbitControls = require('three-orbit-controls')(THREE);
 exports.generate = function () {
   let scene = new THREE.Scene();
   let controls;
+  const ringTilt = Math.PI / 2;
 
-  let camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
+  let ring;
+
+  let camera = new THREE.PerspectiveCamera(42, window.innerWidth / window.innerHeight, 1, 1000);
   let renderer = new THREE.WebGLRenderer({
     alpha: true,
     antialias: true
@@ -18,10 +21,10 @@ exports.generate = function () {
   let geometry = new THREE.SphereGeometry(75, 75, 75);
 
   let material = new THREE.MeshPhongMaterial({
-    color: '#111',
-    opacity: 0.8,
-    shininess: 15,
-    reflectivity: 20,
+    color: '#333',
+    opacity: 1,
+    shininess: 12,
+    reflectivity: 10,
     specular: '#2bd8a3'
   });
 
@@ -36,7 +39,8 @@ exports.generate = function () {
   material = new THREE.MeshBasicMaterial({
     wireframe: true,
     color: '#111',
-    opacity: 0.3
+    transparent: true,
+    opacity: 0.1
   });
 
   sphere = new THREE.Mesh(geometry, material);
@@ -45,32 +49,31 @@ exports.generate = function () {
   sphere.position.z = 0;
   scene.add(sphere);
 
-  geometry = new THREE.SphereGeometry(7, 7, 7);
-
-  material = new THREE.MeshPhongMaterial({
-    color: '#7ae5bb',
-    opacity: 0.8,
-    shininess: 10,
-    reflectivity: 5
-  });
-
-  sphere = new THREE.Mesh(geometry, material);
-  sphere.position.x = -130;
-  sphere.position.y = 10;
-  scene.add(sphere);
-
   geometry = new THREE.SphereGeometry(10, 10, 10);
 
-  material = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    color: '#111',
-    opacity: 0.2
+  material = new THREE.MeshLambertMaterial({
+    color: '#888'
   });
 
   sphere = new THREE.Mesh(geometry, material);
   sphere.position.x = -130;
   sphere.position.y = 10;
   scene.add(sphere);
+
+  geometry = new THREE.RingGeometry(13, 15, 35);
+
+  material = new THREE.MeshPhongMaterial({
+    color: '#eee',
+    opacity: 0.3,
+    shininess: 6,
+    reflectivity: 6
+  });
+
+  ring = new THREE.Mesh(geometry, material);
+  ring.position.x = -130;
+  ring.position.y = 10;
+  ring.rotation.x = -ringTilt;
+  scene.add(ring);
 
   geometry = new THREE.SphereGeometry(16, 16, 16);
 
@@ -87,11 +90,52 @@ exports.generate = function () {
   sphere.position.z = 10;
   scene.add(sphere);
 
+  geometry = new THREE.SphereGeometry(18, 18, 18);
+
+  material = new THREE.MeshPhongMaterial({
+    color: '#cc352a',
+    opacity: 0.7,
+    shininess: 5,
+    reflectivity: 1
+  });
+
+  sphere = new THREE.Mesh(geometry, material);
+  sphere.position.x = 230;
+  sphere.position.y = 5;
+  sphere.position.z = -60;
+  scene.add(sphere);
+
+  function generateRings(start, end) {
+    geometry = new THREE.RingGeometry(start, end, 45);
+
+    material = new THREE.MeshPhongMaterial({
+      color: '#111',
+      opacity: 0.5,
+      shininess: 3,
+      reflectivity: 6
+    });
+
+    ring = new THREE.Mesh(geometry, material);
+    ring.position.x = 230;
+    ring.position.y = 5;
+    ring.position.z = -60;
+    ring.rotation.x = -ringTilt;
+    scene.add(ring);
+  }
+
+  generateRings(19, 21);
+  generateRings(22, 24);
+  generateRings(25, 32);
+  generateRings(34, 50);
+
+  let light = new THREE.HemisphereLight('#222', '#ff1e55', 0.3);
+  scene.add(light);
+
   let directionalLight = new THREE.DirectionalLight('#0078e5', 0.9);
   directionalLight.position.set(1, 1, 1);
   scene.add(directionalLight);
 
-  directionalLight = new THREE.DirectionalLight('#3fa7a3', 0.9);
+  directionalLight = new THREE.DirectionalLight('#3fa7a3', 0.6);
   directionalLight.position.set(1, 0, 1);
   scene.add(directionalLight);
 
